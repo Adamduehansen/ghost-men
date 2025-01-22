d14737d994f406594d8846215f7a020d4fa5a8b9
Adam Due Hansen <adamduehan@gmail.com>
2025-01-22 23:03:42 +0100

    feat: set game properties.

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import "./style.css";
 import { Ghost } from "./ghost";
 import { ControlSchema1 } from "./utils/control-schema";
 
-const engine = new ex.Engine();
+const engine = new ex.Engine({
+  resolution: {
+    width: 224,
+    height: 288,
+  },
+  displayMode: ex.DisplayMode.FitScreen,
+  fixedUpdateFps: 60,
+  pixelRatio: 4,
+  pixelArt: true,
+});
 
 const ghost = new Ghost({
   pos: ex.vec(100, 100),
